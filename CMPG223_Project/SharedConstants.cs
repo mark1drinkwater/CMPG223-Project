7e6f5359185efd83597a00438c9590dd34923ce8
Jacques McDermid Heyns <jmheyns@gmail.com>
2025-10-07 15:51:28 +0200

    Update SharedConstants.cs

--- CMPG223_Project/SharedConstants.cs
+++ CMPG223_Project/SharedConstants.cs
@@ -8,7 +8,7 @@ namespace CMPG223_Project
 {
     public static class SharedConstants
     {
-       // public static string connString = "Data Source=DESKTOP-79PMUBS;Initial Catalog=Shelter;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+        // public static string connString = "Data Source=DESKTOP-79PMUBS;Initial Catalog=Shelter;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
         private static string dataSource = "\"localhost, 1433\"";
         public static string connString = $"Data Source={dataSource};Initial Catalog=Shelter; Persist Security Info=True; User ID=sa;Password=Password!; TrustServerCertificate=True";
     }
